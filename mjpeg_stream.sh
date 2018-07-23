9caeaff070a39a65655daf789156f1193a1641fc
Leonardo Pizarro <lpizarrobaeza@gmail.com>
2018-07-23 17:25:34 -0400

    streaming doesnt need local windows

--- mjpeg_stream.sh
+++ mjpeg_stream.sh
@@ -2,6 +2,6 @@
 #rem Run this file and then open URL in Chrome/Firefox: rem http://localhost:8090
 #rem Or open: http://ip-address:8090
 
-./darknet detector demo ./cfg/coco.data ./cfg/yolov3.cfg ./yolov3.weights $1 -i 0 -thresh 0.25 -http_port 80
+./darknet detector demo ./cfg/coco.data ./cfg/yolov3.cfg ./yolov3.weights $1 -i 0 -thresh 0.25 -http_port 80 -dont_show
 
 #pause
